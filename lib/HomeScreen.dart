8cde6686281048ca070cf8578dd4c26bda32d492
ParthBadkul <badkul190@gmail.com>
2023-04-03 17:37:42 +0530

    weekend

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
+  void fetchdata() async {
+    const url = "https://randomuser.me/api/?results=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    users = json['results'];
+
+    print(users);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[300],
+        body: Center(
+          child: Text("Text"),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: fetchdata),
+      ),
+    );
   }
 }
