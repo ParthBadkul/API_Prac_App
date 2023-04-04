095c80a0ffd0a184aa6ccb8d5e76ee4d02896e81
ParthBadkul <badkul190@gmail.com>
2023-04-04 14:52:00 +0530

    adding images

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,7 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> users = [];
 
   void fetchdata() async {
-    const url = "https://randomuser.me/api/?results=10";
+    const url = "https://randomuser.me/api/?results=100";
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
@@ -30,8 +30,19 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         backgroundColor: Colors.deepPurple[300],
         body: Center(
-          child: Text("Text"),
-        ),
+            child: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(users[index]['name']['first'].toString()),
+              leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                      users[index]['picture']['thumbnail'].toString())),
+              subtitle: Text(users[index]['email']),
+            );
+          },
+        )),
         floatingActionButton: FloatingActionButton(onPressed: fetchdata),
       ),
     );
